770458d02f5cf548b548843c19862809618d330c
Daniel Daugbjerg <ddaugbjerg@sandiego.edu>
2022-05-12 16:46:49 -0700

    deleted old files

--- wordyiteration2.py
+++ wordyiteration2.py
@@ -268,21 +268,20 @@ class Iteration2Helper:
                     return
             else:
                 # Initialize parameter variables and disable checkboxes/entries
-                #self.hard_mode_parameter = self.hard_mode_checkbox_var.get()
+                
                 self.hard_mode_checkbutton['state'] = 'disabled'
-                #self.guesses_must_be_words_parameter = self.guesses_must_be_words_checkbox_var.get()
+                
                 self.guesses_must_be_words_checkbutton['state'] = 'disabled'
-                #self.show_word_parameter = self.show_word_checkbox_var.get()
-                #self.hidden_word = self.specify_word_entry_var.get()
+                
                 self.specify_word_checkbutton['state'] = 'disabled'
                 self.specify_word_entry_var.set("")
         else:
             # Initialize parameter variables and disable checkboxes/entries
-            #self.hard_mode_parameter = self.hard_mode_checkbox_var.get()
+            
             self.hard_mode_checkbutton['state'] = 'disabled'
-            #self.guesses_must_be_words_parameter = self.guesses_must_be_words_checkbox_var.get()
+            
             self.guesses_must_be_words_checkbutton['state'] = 'disabled'
-            #self.show_word_parameter = self.show_word_checkbox_var.get()
+            
             self.specify_word_checkbutton['state'] = 'disabled'
             # Takes random word from long_list
             self.hidden_word = self.long_list[random.randint(0, len(self.short_list))]
